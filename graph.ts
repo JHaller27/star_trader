28588bad9ecb30a28b5d9bc9afae86ca37e7d6fd
james <jameshaller27@gmail.com>
2020-03-08 12:38:15 -0700

    Create RouteMap

--- graph.ts
+++ graph.ts
@@ -1,5 +1,33 @@
 import { Port } from "./port";
-import { TradeInfo } from "./commodity";
+import { TradeInfo, Commodity } from "./commodity";
+
+export class RouteMap {
+    private readonly portMap: Map<Port, PortNode>;
+
+    constructor() {
+        this.portMap = new Map();
+    }
+
+    public addPort(port: Port, tradeInfo: TradeInfo): void {
+        const node = new PortNode(port, tradeInfo);
+        this.portMap.set(port, node);
+    }
+
+    public addRoute(origin: Port, destination: Port, commodity: Commodity): void {
+        const originNode = this.portMap.get(origin);
+        const destinationNode = this.portMap.get(destination);
+
+        if (originNode === undefined) {
+            throw new Error(`Origin port ${origin.toString()} not found. Make sure to add Port to RouteMap first.`);
+        }
+        if (destinationNode === undefined) {
+            throw new Error(`Destination port ${destination.toString()} not found. Make sure to add Port to RouteMap first.`);
+        }
+
+        const route = new Route(destinationNode, commodity.hash());
+        originNode.addRoute(route);
+    }
+}
 
 export class PortNode {
     private readonly port: Port;
@@ -10,6 +38,10 @@ export class PortNode {
         this.port = port;
         this.tradeInfo = tradeInfo;
         this.routes = [];
+    }
+
+    public addRoute(route: Route): void {
+        this.routes.push(route);
     }
 }
 
