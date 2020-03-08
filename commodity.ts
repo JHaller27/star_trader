da6144ab66151ce71005117fb79c6a783b73192d
james <jameshaller27@gmail.com>
2020-03-07 20:55:34 -0800

    Add Commodity class

---
+++ commodity.ts
@@ -0,0 +1,9 @@
+export class Commodity {
+    private readonly name: string;
+    private readonly price: number;
+
+    constructor(name: string, price: number) {
+        this.name = name;
+        this.price = price;
+    }
+}
