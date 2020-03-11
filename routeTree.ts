10a9e7a24e49b7958c9f6a1b720956a2b21d6cdc
james <jameshaller27@gmail.com>
2020-03-10 23:48:22 -0700

    Add TreeNode and method skeleton

--- routeTree.ts
+++ routeTree.ts
@@ -1,2 +1,19 @@
+class TreeNode {
+    public maxProfit(): number {
+        return 0;
+    }
+
+    public addChild(childNode: TreeNode, profit: number): void {
+    }
+
+    public getChildren(): TreeNode[] {
+        return [];
+    }
+
+    public getParent(): TreeNode | undefined {
+        return undefined;
+    }
+}
+
 class RouteTree {
 }
