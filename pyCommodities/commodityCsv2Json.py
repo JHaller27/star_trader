93fd8cf270d409795c767433189ebbb61c824820
James <jameshaller27@gmail.com>
2020-03-13 00:16:59 -0700

    Use argparser and allow --no-vice

--- pyCommodities/commodityCsv2Json.py
+++ pyCommodities/commodityCsv2Json.py
@@ -1,13 +1,14 @@
 import csv
 import json
-import sys
+import argparse
 
-if len(sys.argv) < 3:
-    print('Usage: csv_path json_path', file=sys.stderr)
-    exit(1)
+parser = argparse.ArgumentParser()
 
-csv_in_path = sys.argv[1]
-json_out_path = sys.argv[2]
+parser.add_argument('csv_path', type=str, help='Path to CSV to read from')
+parser.add_argument('json_path', type=str, help='Path to JSON to write to')
+parser.add_argument('--no-vice', '-v', dest='novice', action='store_true', help='Set this flag to exclude vice')
+
+args = parser.parse_args()
 
 commodities = []
 
@@ -23,20 +24,22 @@ def convert_row(line) -> list:
                 row.append(el)
     return row
 
-def buySellLines(itr) -> (list, list):
+def buySellLines(itr) -> (str, list, list):
     '''
-    Return (buy, sell)
+    Return (name, buy, sell)
     '''
     while True:
         try:
             buy = convert_row(next(itr))
             sell = convert_row(next(itr))
 
-            yield buy, sell
+            name = buy[0]
+
+            yield name, buy, sell
         except StopIteration:
             return
 
-with open(csv_in_path, newline='') as fin:
+with open(args.csv_path, newline='') as fin:
     reader = csv.reader(fin)
 
     i = iter(reader)
@@ -48,12 +51,13 @@ with open(csv_in_path, newline='') as fin:
             'commodities': []
         })
 
-    for buy_prices, sell_prices in buySellLines(i):
+    for name, buy_prices, sell_prices in buySellLines(i):
+        if args.novice and 'vice' in name.lower():
+            continue
 
         # Swap buy to selling and sell to buying due to semantics
         #   CSV assumes perspective of player (e.g. 'player can buy this')
         #   JSON assumes perspective of port (e.g. 'port is selling this')
-        name = buy_prices[0]
         for pidx in range(len(buy_prices) - 1):
             buy = buy_prices[pidx+1]
             sell = sell_prices[pidx+1]
@@ -67,5 +71,5 @@ with open(csv_in_path, newline='') as fin:
 
 json_text = json.JSONEncoder(indent=4).encode(commodities)
 
-with open(json_out_path, 'w') as fout:
+with open(args.json_path, 'w') as fout:
     fout.write(json_text)
